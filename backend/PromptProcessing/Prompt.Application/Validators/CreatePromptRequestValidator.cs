6cab317274845f4cba9729dc96ca971d03d2ae91
Kamil Plekaniec <kamil.plekaniec@romico.de>
2026-07-05 17:12:31 +0200

    refactor

---
+++ backend/PromptProcessing/Prompt.Application/Validators/CreatePromptRequestValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using Prompt.Application.DTOs;
+
+namespace Prompt.Application.Validators;
+
+public class CreatePromptRequestValidator : AbstractValidator<CreatePromptRequest>
+{
+    public CreatePromptRequestValidator()
+    {
+        RuleFor(x => x.Prompt)
+            .NotEmpty()
+            .MaximumLength(4000);
+    }
+}
